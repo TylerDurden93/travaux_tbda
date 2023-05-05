c73a1997718fea5ea1bd03273ad891e24fe169e4
chiboub fayçal <f.chiboub@hotmail.fr>
2023-05-05 13:29:38 +0200

    test JS

---
+++ js/js.js
@@ -0,0 +1,15 @@
+{
+    
+var bienvenu = "vous allez devoir repondre a des question" ;
+var prenom = "saisissez votre prenom" ;
+var age ="saisissez votre age";
+
+}
+
+
+
+    alert(bienvenu);
+if (confirm ("etes vous d'accord ?") ==true);
+    prompt(prenom);
+    prompt(age);
+
